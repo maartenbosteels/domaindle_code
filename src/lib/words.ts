33919a7b68be342c4db19903c223c8e1ad578fba
Maarten Bosteels <maarten.bosteels@dnsbelgium.be>
2022-03-10 10:22:08 +0100

    Check if delegated

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -1,14 +1,73 @@
 import { WORDS } from '../constants/wordlist'
-import { VALID_GUESSES } from '../constants/validGuesses'
 import { WRONG_SPOT_MESSAGE, NOT_CONTAINED_MESSAGE } from '../constants/strings'
 import { getGuessStatuses } from './statuses'
 
-export const isWordInWordList = (word: string) => {
-  return (
-    WORDS.includes(word.toLowerCase()) ||
-    VALID_GUESSES.includes(word.toLowerCase())
-  )
+export const isWordInWordList = async (word: string) => {
+  if (solution === word) {
+    return true
+  }
+  console.log("checking " + word);
+  let registered = await isRegistered(word);
+  console.log("registered: " + registered)
+  return registered;
+
 }
+
+export const isRegistered = async (word: string) => {
+  console.log("checking " + word.toLowerCase());
+  // https://api.dnsbelgium.be/whois/registration/abc/status
+  // let url = "https://api.dnsbelgium.be/whois/registration/" + word.toLowerCase() + "/status"
+
+  let domainName = word.toLowerCase();
+  let delegated = await isDelegated(domainName);
+  if (delegated) {
+    return true;
+  }
+
+  let url = "https://api.dnsbelgium.be/pubws/das?domain=" + domainName
+
+  console.log("checking with url: " + url);
+
+  // noinspection UnnecessaryLocalVariableJS
+  let registered = fetch(url)
+    .then(value => value.json())
+    .then(value => value.data.dasResults.be.status)
+    .then(value => {
+      let domainStatus = value.toString()
+      console.log("domain status : [" + value.toString() + "]")
+      if (domainStatus === "DELEGATED") {
+        console.log("domain [" + word + "] is registered")
+        return true;
+      } else {
+        console.log("domain [" + word + "] is NOT registered")
+        return false;
+      }
+    });
+  //console.log("registered: " + registered)
+  return registered;
+
+}
+
+export const isDelegated = async (word: string) => {
+  const domainName = word.toLowerCase() + ".be";
+  let url = 'https://dns.google/resolve?name=' + domainName;
+  var response = await fetch(url);
+  var json = await response.json();
+  console.log(json);
+  return json.Status === 0;
+}
+
+export const isDnssecSigned = async (word: string) => {
+  const domainName = word.toLowerCase() + ".be";
+  let url = 'https://dns.google/resolve?name=' + domainName  + "&type=DS"
+  var response = await fetch(url);
+  var json = await response.json();
+  console.log(json);
+  const signed = json.Status === 0 && json.AD === true;
+  console.log("DNSSEC signed: " + signed);
+  return signed;
+}
+
 
 export const isWinningWord = (word: string) => {
   return solution === word
@@ -54,10 +113,16 @@ export const findFirstUnusedReveal = (word: string, guesses: string[]) => {
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
   const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
-  const now = Date.now()
+  const now = Date.now() + 86400000
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
+  // console.log('getWordOfDay WORDS.length=' + WORDS.length)
+  // console.log('getWordOfDay now=' + now)
+  // console.log('getWordOfDay index=' + index)
+  // console.log('getWordOfDay nextday=' + nextday)
+  // const solution = WORDS[index % WORDS.length].toUpperCase();
+  // console.log('solution index=' + solution)
 
   return {
     solution: WORDS[index % WORDS.length].toUpperCase(),
